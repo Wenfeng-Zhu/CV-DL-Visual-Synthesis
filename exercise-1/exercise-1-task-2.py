e9ffddb1932168ef3077891c05ff35046cd0891c
Wenfeng Zhu <zhuwenfengerik@outlook.com>
2022-05-02 23:42:03 +0200

    finish laplacian filter

--- exercise-1/exercise-1-task-2.py
+++ exercise-1/exercise-1-task-2.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.image as image
 import matplotlib.pyplot as plt
+import cv2
 
 
 def grayscale(img):
@@ -15,7 +16,6 @@ def grayscale(img):
 
 
 def convolution_operation(imgGray, conv_filter):
-    print(conv_filter)
     height, width = imgGray.shape
     size = len(conv_filter)
     # create a new numpy array with padding to make sure the filtered image has the same size as the input image
@@ -49,21 +49,63 @@ def gaussian_kernel(kernel_size, mean, variance):
     return kernel
 
 
-# def laplacian_filter(kernel_size):
+def laplacian_of_gaussian(x, y, sigma):
+    # Formatted this way for readability
+    nom = (y ** 2) + (x ** 2) - 2 * (sigma ** 2)
+    denom = 2 * np.pi * (sigma ** 6)
+    expo = np.exp(-((x ** 2) + (y ** 2)) / (2 * (sigma ** 2)))
+    return (nom * expo / denom)
+
+
+def create_log(sigma, kernel_size):
+    w = (np.ceil(float(kernel_size) * float(sigma)))
+    if w % 2 == 0:
+        w = w + 1
+    l_o_g_mask = []
+    w_range = int(np.floor(w / 2))
+    print("Going from " + str(-w_range) + " to " + str(w_range))
+    for i in range(-w_range, w_range + 1):
+        for j in range(-w_range, w_range + 1):
+            l_o_g_mask.append(laplacian_of_gaussian(i, j, sigma))
+    print(len(l_o_g_mask))
+    l_o_g_mask = np.array(l_o_g_mask)
+    print(l_o_g_mask)
+    l_o_g_mask = l_o_g_mask.reshape(int(w), int(w))
+    return l_o_g_mask
+
+
+def laplacian_filter(imgGray, kernel_size, sigma):
+    log_mask = create_log(sigma, kernel_size)
+    img_log = convolution_operation(imgGray, log_mask)
+    norm = (img_log - np.min(img_log)) / (np.max(img_log) - np.min(img_log))
+    img_log = img_log * norm * 255
+    plt.imshow(img_log, cmap="gray")
+    plt.show()
+    plt.clf()
+    return img_log
 
 
 img = image.imread('dog.jpg')
-print(img.shape)
 imgGray = grayscale(img)
-filter_1 = np.asarray([[1, 2],
-                       [2, 1]])
+filter_1 = np.asarray([[0, 1, 0], [1, -4, 1], [0, 1, 0]])
 gaussian_filter = gaussian_kernel(200, [60, 100], 30)
-plt.imshow(gaussian_filter, cmap='gray')
-plt.title('Gaussian Filter of 200x200 size')
-plt.show()
+# plt.imshow(gaussian_filter, cmap='gray')
+# plt.title('Gaussian Filter of 200x200 size')
+# plt.show()
 img_conv_gaussian = convolution_operation(imgGray, gaussian_kernel(7, [4, 3], 30))
-print(img_conv_gaussian)
-plt.imshow(img_conv_gaussian, cmap='gray')
-print(img_conv_gaussian.shape)
-plt.title('blurred image of Dog image')
+# plt.imshow(img_conv_gaussian, cmap='gray')
+# plt.title('blurred image of Dog image')
+# plt.show()
+# laplacian_kernel = laplacian_filter(3, 2)
+img_laplacian = laplacian_filter(imgGray, 7, 1)
+# plt.imshow(img_laplacian, cmap='gray')
+# plt.title('Sharpening image of Dog image')
+# print(img_laplacian)
+# plt.show()
+source = cv2.imread('dog.jpg', cv2.IMREAD_COLOR)
+source = cv2.GaussianBlur(source, (3, 3), 0)
+source_gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+dest = cv2.Laplacian(source_gray, cv2.CV_16S, ksize=3)
+abs_dest = cv2.convertScaleAbs(dest)
+plt.imshow(abs_dest, cmap="gray")
 plt.show()
