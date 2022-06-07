91ab50f0440bd6199a0295e0d7de484c3bb41da2
Wenfeng Zhu <zhuwenfengerik@outlook.com>
2022-06-07 22:35:48 +0200

    finish the task1-task4 of exercise-6

---
+++ exercise-6/zhu-12343074.py
@@ -0,0 +1,205 @@
+import numpy as np
+import os
+import torch
+import torchvision.models
+from torch.optim import Adam
+import copy
+from PIL import Image
+import matplotlib.cm as mpl_color_map
+from matplotlib.colors import ListedColormap
+from matplotlib import pyplot as plt
+from torch.autograd import Variable
+
+# from  aux_ops import preprocess_image, recreate_image, save_image
+# Initialize GPU if available
+use_gpu = False
+if torch.cuda.is_available():
+    use_gpu = True
+# Select device to work on.
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def format_np_output(np_arr):
+    """
+        This is a (kind of) bandaid fix to streamline saving procedure.
+        It converts all the outputs to the same format which is 3xWxH
+        with using sucecssive if clauses.
+    Args:
+        im_as_arr (Numpy array): Matrix of shape 1xWxH or WxH or 3xWxH
+    """
+    # Phase/Case 1: The np arr only has 2 dimensions
+    # Result: Add a dimension at the beginning
+    if len(np_arr.shape) == 2:
+        np_arr = np.expand_dims(np_arr, axis=0)
+    # Phase/Case 2: Np arr has only 1 channel (assuming first dim is channel)
+    # Result: Repeat first channel and convert 1xWxH to 3xWxH
+    if np_arr.shape[0] == 1:
+        np_arr = np.repeat(np_arr, 3, axis=0)
+    # Phase/Case 3: Np arr is of shape 3xWxH
+    # Result: Convert it to WxHx3 in order to make it saveable by PIL
+    if np_arr.shape[0] == 3:
+        np_arr = np_arr.transpose(1, 2, 0)
+    # Phase/Case 4: NP arr is normalized between 0-1
+    # Result: Multiply with 255 and change type to make it saveable by PIL
+    if np.max(np_arr) <= 1:
+        np_arr = (np_arr * 255).astype(np.uint8)
+    return np_arr
+
+
+def save_image(im, path):
+    """
+        Saves a numpy matrix or PIL image as an image
+    Args:
+        im_as_arr (Numpy array): Matrix of shape DxWxH
+        path (str): Path to the image
+    """
+    if isinstance(im, (np.ndarray, np.generic)):
+        im = format_np_output(im)
+        im = Image.fromarray(im)
+    im.save(path)
+
+
+def preprocess_image(pil_im, resize_im=True):
+    """
+        Processes image for CNNs
+
+    Args:
+        PIL_img (PIL_img): PIL Image or numpy array to process
+        resize_im (bool): Resize to 224 or not
+    returns:
+        im_as_var (torch variable): Variable that contains processed float tensor
+    """
+    # Mean and std list for channels (Imagenet)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+
+    # Ensure or transform incoming image to PIL image
+    if type(pil_im) != Image.Image:
+        try:
+            pil_im = Image.fromarray(pil_im)
+        except Exception as e:
+            print("could not transform PIL_img to a PIL Image object. Please check input.")
+
+    # Resize image
+    if resize_im:
+        pil_im = pil_im.resize((224, 224), Image.ANTIALIAS)
+
+    im_as_arr = np.float32(pil_im)
+    im_as_arr = im_as_arr.transpose(2, 0, 1)  # Convert array to D,W,H
+    # Normalize the channels
+    for channel, _ in enumerate(im_as_arr):
+        im_as_arr[channel] /= 255
+        im_as_arr[channel] -= mean[channel]
+        im_as_arr[channel] /= std[channel]
+    # Convert to float tensor
+    im_as_ten = torch.from_numpy(im_as_arr).float()
+    # Add one more channel to the beginning. Tensor shape = 1,3,224,224
+    im_as_ten.unsqueeze_(0)
+    # Convert to Pytorch variable
+    im_as_var = Variable(im_as_ten, requires_grad=True)
+    return im_as_var
+
+
+def recreate_image(im_as_var):
+    """
+        Recreates images from a torch variable, sort of reverse preprocessing
+    Args:
+        im_as_var (torch variable): Image to recreate
+    returns:
+        recreated_im (numpy arr): Recreated image in array
+    """
+    reverse_mean = [-0.485, -0.456, -0.406]
+    reverse_std = [1 / 0.229, 1 / 0.224, 1 / 0.225]
+    recreated_im = copy.copy(im_as_var.data.numpy()[0])
+    for c in range(3):
+        recreated_im[c] /= reverse_std[c]
+        recreated_im[c] -= reverse_mean[c]
+    recreated_im[recreated_im > 1] = 1
+    recreated_im[recreated_im < 0] = 0
+    recreated_im = np.round(recreated_im * 255)
+
+    recreated_im = np.uint8(recreated_im).transpose(1, 2, 0)
+    return recreated_im
+
+
+def load_vgg16():
+    vgg16 = torchvision.models.vgg16(pretrained=True).to(device)
+    print(vgg16)
+
+
+# Total Variation Loss
+def total_variation_loss(img, weight):
+    # Your code here
+    pass
+
+
+def visualise_layer_filter(model, layer_nmbr, filter_nmbr, num_optim_steps=26):
+    # Generate a random image
+    rand_img = np.uint8(np.random.uniform(low=120,
+                                          high=190,
+                                          size=(224, 224, 3)))
+
+    # Process image and return variable
+    processed_image = preprocess_image(rand_img, False)
+    processed_image = torch.tensor(processed_image, device=device).float()
+    processed_image.requires_grad = True
+    # Define optimizer for the image
+    optimizer = Adam([processed_image], lr=0.1, weight_decay=1e-5)
+    for i in range(1, num_optim_steps):
+        optimizer.zero_grad()
+        # Assign create image to a variable to move forward in the model
+        x = processed_image
+        for index, layer in enumerate(model):
+            # Forward pass layer by layer
+            x = layer(x)
+            if index == layer_nmbr: break
+
+        conv_output = x[0, filter_nmbr]
+        # Loss function is the mean of the output of the selected layer/filter
+        # We try to minimize the mean of the output of that specific filter
+
+        loss = -torch.mean(conv_output)
+
+        # You may need to add total variation loss later
+        # loss_tv = total_variation_loss(processed_image, 500.)
+        # loss = -torch.mean(conv_output) + loss_tv*1.
+
+        print(f'Step {i:05d}. Loss:{loss.data.cpu().numpy():0.2f}')
+        # Compute gradients
+        loss.backward()
+        # Apply gradients
+        optimizer.step()
+        # Recreate image
+
+        # Save image
+        if i % 5 == 0:
+            optimized_image = recreate_image(processed_image.cpu())
+            im_path = './output_filters_visualization/img' + str(layer_nmbr) + \
+                      '_f' + str(filter_nmbr) + '_iter' + str(i) + '.jpg'
+            save_image(optimized_image, im_path)
+
+
+if __name__ == "__main__":
+    load_vgg16()
+
+    # layer_nmbr = 28
+    # filter_nmbr = 228
+
+    # Fully connected layer is not needed
+    model = torchvision.models.vgg16(pretrained=True).features.to(device)
+    model.eval()
+    # Fix model weights
+    for param in model.parameters():
+        param.requires_grad = False
+    # Enable GPU
+    if use_gpu:
+        model.cuda()
+
+    # Create the folder to export images if not exists
+    if not os.path.exists('./output_filters_visualization'):
+        os.makedirs('./output_filters_visualization')
+
+    visualise_layer_filter(model, 28, 228)
+    visualise_layer_filter(model, 13, 100)
+    visualise_layer_filter(model, 5, 100)
+    visualise_layer_filter(model, 28, 120)
